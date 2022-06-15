507b3276f6a3ce6b7f8c1e0873e12c31475791ab
Md. Shoeb Adnan <madnan172095@bscse.uiu.ac.bd>
2022-06-15 13:09:13 +0600

    Created a custom image view for single comic

--- lib/View/Widgets/custom_image_view.dart
+++ lib/View/Widgets/custom_image_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:full_screen_image_null_safe/full_screen_image_null_safe.dart';
+
 import 'package:xkcd/View/Widgets/custom_progress.dart';
 
 class ImageView extends StatelessWidget {
@@ -12,11 +15,26 @@ class ImageView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return FullScreenWidget(
       child: Container(
         //height: heightMain * .5,
         width: double.infinity,
-        
+        child: InteractiveViewer(
+          panEnabled: false, // Set it to false to prevent panning.
+          boundaryMargin: EdgeInsets.all(0),
+
+          minScale: 0.1,
+          maxScale: 4,
+          child: CachedNetworkImage(
+            fit: BoxFit.contain,
+            imageUrl: comicImg,
+            progressIndicatorBuilder: (context, url, downloadProgress) =>
+                Center(
+              child: customProgressWidget(),
+            ),
+            errorWidget: (context, url, error) => Icon(Icons.error),
+          ),
+        ),
       ),
     );
   }
